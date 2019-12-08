0b29d195b4496ba1242428b67be660660a34b923
wangpeng <wangpeng@adnice.com>
2019-12-08 11:26:57 +0800

    init db

---
+++ backend/src/main/java/com/tt/sale/goods/entity/Account.java
@@ -0,0 +1,61 @@
+package com.tt.sale.goods.entity;
+
+import java.util.Date;
+
+public class Account {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private Integer status;
+    private Date createTime;
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
